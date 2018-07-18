362eacc8f17d53956617180eb0571831a139454d
Fabio Gallonetto <fabio.gallonetto@schibsted.com>
2018-07-18 20:50:57 +0100

    Add install script to set the loading gpio color

--- plugins/miscellanea/ampstatusled/install.sh
+++ plugins/miscellanea/ampstatusled/install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+gpio_regex="^gpio=[0-9]{1,2}=op,dh"
+custom_gpio_conf="gpio=22=op,dh"
+if grep -E "${gpio_regex}" /boot/config.txt; then
+  sed -i'.bak' -E "s/${gpio_regex}/${custom_gpio_conf}/" /boot/config.txt
+else
+  echo "${custom_gpio_conf}" >> /boot/config.txt
+fi
 
 #requred to end the plugin install
 echo "plugininstallend"
